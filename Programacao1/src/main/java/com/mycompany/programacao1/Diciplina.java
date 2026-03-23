c72daa68fbb284e0338098da652dc926d888bf9e
aluno <aluno@lab01-14>
2026-03-23 08:46:52 -0300

    atualizacao3

--- Programacao1/src/main/java/com/mycompany/programacao1/Diciplina.java
+++ Programacao1/src/main/java/com/mycompany/programacao1/Diciplina.java
@@ -15,4 +15,39 @@ public class Diciplina {
     private String ementa;
     private String objetivos;
     
+    public String getCodigoDiciplina() {
+        return codigoDiciplina;
+    }
+    
+    public void setCodigoDiciplina(String _codigoDiciplina) {
+        codigoDiciplina = _codigoDiciplina;
+    }
+    
+    public Diciplina() {
+        nomeDiciplina = "Nobody";
+    }
+    
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+    
+    public void setCargaHoraria(String _cargaHoraria) {
+        cargaHoraria = _cargaHoraria;
+    }
+    
+    public String getEmenta() {
+        return ementa;
+    }
+    
+    public void setEmenta(String _ementa) {
+        ementa = _ementa;
+    }
+    
+    public String getObjetivos() {
+        return objetivos;
+    }
+    
+    public void setObjetivos(String _ojetivos) {
+        objetivos = _ojetivos;
+    }
 }
