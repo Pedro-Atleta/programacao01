5ebc68e2dc1bfd1b701dc06bbb88c76c46f6d574
aluno <aluno@lab01-14>
2026-03-23 08:46:43 -0300

    atualizacao3

--- Programacao1/src/main/java/com/mycompany/programacao1/Professor.java
+++ Programacao1/src/main/java/com/mycompany/programacao1/Professor.java
@@ -18,8 +18,82 @@ public class Professor {
     private String titulacao;
     private String areadeAtuacao;
     private String regimeTrabalho;
-    private String anoIngresso;
+    private int anoIngresso;
     private String formacao;
     
+    public Professor() {
+        nomeProfessor = "Nobody";
+    }
     
+    public String getnomeProfessor() {
+        return nomeProfessor;
+    }
+    
+    public void setNome(String _nome) {
+        nomeProfessor = _nome;
+    }
+    
+    public String  getIAPE() {
+        return SIAPE;
+    }
+    
+    public void setIAPE(String _SIAPE) {
+        SIAPE = _SIAPE;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String _email) {
+        email = _email;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    public void setTelefone(String _telefone) {
+        telefone = _telefone;
+    }
+    
+    public String getTitulacao() {
+        return titulacao;
+    }
+   
+    public void setTitulacao(String _titulacao) {
+        titulacao = _titulacao;
+    }
+    
+    public String getAreaAtuacao() {
+        return areadeAtuacao;
+    }
+    
+    public void setAreaAtuacao(String _areaAtuacao) {
+        areadeAtuacao = _areaAtuacao;
+    }
+    
+    public String getRegimeTrabalho() {
+        return regimeTrabalho;
+    }
+    
+    public void setRegimeTrabalho(String _regimeTrabalho) {
+        regimeTrabalho = _regimeTrabalho;
+    }
+    
+    public int getanoIngresso() {
+        return anoIngresso;
+    }
+    
+    public void setanoIngresso(int _anoIngresso) {
+        anoIngresso = _anoIngresso;
+    }
+    
+     public String getformacao() {
+        return formacao;
+    }
+    
+    public void setformacao(String _formacao) {
+        formacao = _formacao;
+    }
 }
